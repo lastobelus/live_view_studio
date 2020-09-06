67fdd460c00fd11f50887faace1fb23e3a2265f1
Michael Johnston <lastobelus@mac.com>
2020-09-05 18:51:08 -0700

    implements color temperature selector with radio buttons

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -6,7 +6,11 @@ defmodule LiveViewStudioWeb.LightLive do
   # params: map containing query & router params
   def mount(_params, _session, socket) do
     IO.puts "MOUNT #{inspect(self())}"
-    socket = assign(socket, :brightness, 10)
+    socket = assign(
+      socket,
+      brightness: 10,
+      temp: 5000
+    )
     # IO.inspect socket
     {:ok, socket}
   end
@@ -16,8 +20,8 @@ defmodule LiveViewStudioWeb.LightLive do
     ~L"""
     <h1>Front Porch Light</h1>
     <div id="light">
-      <div class="meter">
-        <span style="width: <%= @brightness %>%">
+      <div class="meter" >
+        <span style="width: <%= @brightness %>%; background-color: <%= temp_color(@temp) %>;">
           <%= @brightness %>%
         </span>
 
@@ -46,6 +50,16 @@ defmodule LiveViewStudioWeb.LightLive do
         <form phx-change="update">
           <input type="range" min="0" max="100"
                 name="brightness" value="<%= @brightness %>" />
+
+          <div class="px-5 py-2 mt-3 bg-gray-300 rounded-lg">
+            <strong class="mr-2 font-bold">Temperature</strong>
+            <input type="radio" id="3000" name="temp" value="3000" <%= if 3000 == @temp, do: "checked" %>/>
+            <label for="3000">3000</label>
+            <input type="radio" id="4000" name="temp" value="4000" <%= if 4000 == @temp, do: "checked" %> />
+            <label for="4000">4000</label>
+            <input type="radio" id="5000" name="temp" value="5000" <%= if 5000 == @temp, do: "checked" %> />
+            <label for="5000">5000</label>
+          </div>
         </form>
       </div>
 
@@ -95,12 +109,31 @@ defmodule LiveViewStudioWeb.LightLive do
   end
 
 
-    def handle_event("update", %{"brightness" => brightness}, socket) do
+    def handle_event("update", %{"brightness" => brightness, "temp" => temp}, socket) do
     # &() is shorthand capture syntax for creating an anonymous function
     # function will receive the current value of the socket key
     # &1 represents first argument to function
     brightness = String.to_integer(brightness)
-    socket = assign(socket, :brightness, brightness)
+        temp = String.to_integer(temp)
+
+    socket = assign(
+      socket,
+      brightness: brightness,
+      temp: temp
+    )
     {:noreply, socket}
   end
+
+  def handle_event("update", %{"temp" => temp}, socket) do
+    # &() is shorthand capture syntax for creating an anonymous function
+    # function will receive the current value of the socket key
+    # &1 represents first argument to function
+    temp = String.to_integer(temp)
+    socket = assign(socket, :temp, temp)
+    {:noreply, socket}
+  end
+
+  defp temp_color(3000), do: "#F1C40D"
+  defp temp_color(4000), do: "#FEFF66"
+  defp temp_color(5000), do: "#99CCFF"
 end
