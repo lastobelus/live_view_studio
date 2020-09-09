fbad6b2da2e20b219119dfc6e33cfa630e990956
Michael Johnston <lastobelus@mac.com>
2020-09-09 11:25:18 -0700

    add search flights by airport code, with autocomplete for airports
    used a handle_info that receives a search function to DRY events
    don't search when flight_number or airport are blank

--- lib/live_view_studio_web/live/flights_live.ex
+++ lib/live_view_studio_web/live/flights_live.ex
@@ -2,11 +2,15 @@ defmodule LiveViewStudioWeb.FlightsLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Flights
+  alias LiveViewStudio.Airports
+
 
   def mount(_params, _session, socket) do
     socket = assign(
       socket,
       flight_number: "",
+      airport: "",
+      airport_matches: [],
       flights: Flights.list_flights(),
       loading: false
     )
@@ -18,16 +22,39 @@ defmodule LiveViewStudioWeb.FlightsLive do
     <h1>Find a Flight</h1>
 
     <div id="search">
-      <form phx-submit="flight_number_search">
-        <input type="text" name="flight_number" value="<%= @flight_number %>"
-              autofocus autocomplete="off"
-              placeholder=""
-              <%= if @loading, do: "readonly" %>
-              />
-        <button type="submit">
-          <img src="images/search.svg">
-        </button>
-      </form>
+      <div class="flex justify-center">
+        <form phx-submit="flight-number-search" class="m-3">
+          <input type="text" name="flight_number" value="<%= @flight_number %>"
+                autofocus autocomplete="off"
+                placeholder="Flight Number"
+                <%= if @loading, do: "readonly" %>
+                />
+          <button type="submit">
+            <img src="images/search.svg">
+          </button>
+        </form>
+
+        <form phx-submit="airport-search" phx-change="suggest-airport" class="m-3">
+          <input type="text" name="airport" value="<%= @airport %>"
+                autocomplete="off"
+                placeholder="Airport Code"
+                list="airport_list"
+                debounce="250"
+                <%= if @loading, do: "readonly" %>
+                />
+          <button type="submit">
+            <img src="images/search.svg">
+          </button>
+
+        </form>
+      </div>
+
+
+      <datalist id="airport_list">
+        <%= for airport_match <- @airport_matches do %>
+          <option value="<%= airport_match %>"><%= airport_match %></option>
+        <% end %>
+      </datalist>
 
       <%= if @loading do %>
         <div class="loader">Loading...</div>
@@ -63,23 +90,47 @@ defmodule LiveViewStudioWeb.FlightsLive do
     """
   end
 
-  def handle_event("flight_number_search", %{"flight_number" => flight_number}, socket) do
-    send(self(), {:run_flight_number_search, flight_number})
+  def handle_event("flight-number-search", %{"flight_number" => flight_number}, socket) do
+    if not is_blank?(flight_number) do
+      send(self(), {:run_search, &Flights.search_by_number/1, flight_number})
+      socket = assign(
+        socket,
+        loading: true,
+        flight_number: flight_number,
+        flights: []
+      )
+    end
+    {:noreply, socket}
+  end
+
+  def handle_event("suggest-airport", %{"airport" => airport}, socket) do
     socket = assign(
       socket,
-      loading: true,
-      flight_number: flight_number,
-      flights: []
+      airport_matches: Airports.suggest(airport)
     )
     {:noreply, socket}
   end
 
-  def handle_info({:run_flight_number_search, flight_number}, socket) do
-    case Flights.search_by_number(flight_number) do
+  def handle_event("airport-search", %{"airport" => airport}, socket) do
+    if not is_blank?(airport) do
+      send(self(), {:run_search, &Flights.search_by_airport/1, airport})
+      socket = assign(
+        socket,
+        loading: true,
+        airport: airport,
+        flights: []
+      )
+    end
+    {:noreply, socket}
+  end
+
+
+  def handle_info({:run_search, search_fn, query}, socket) do
+    case search_fn.(query) do
       [] ->
         socket =
           socket
-          |> put_flash(:error, "No flights matching \"#{flight_number}\"")
+          |> put_flash(:error, "No flights matching \"#{query}\"")
           |> assign(loading: false, flights: [])
         {:noreply, socket}
 
@@ -90,7 +141,9 @@ defmodule LiveViewStudioWeb.FlightsLive do
           |> assign(loading: false, flights: flights)
         {:noreply, socket}
     end
+  end
 
-
+  defp is_blank? value do
+    "" == value |> to_string() |> String.trim()
   end
 end
